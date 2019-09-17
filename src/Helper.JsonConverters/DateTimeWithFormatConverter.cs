8114972f0273eb7469d0cf25f4e87d2a75a0d846
Roberto Camara <robertocamara@outlook.com.br>
2019-09-17 10:09:26 -0300

    add new converter

---
+++ src/Helper.JsonConverters/DateTimeWithFormatConverter.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Helper.JsonConverters
+{
+    /// <summary>
+    /// Custom DateTime with JSON Serializer / Deserializer Formatting
+    /// </summary>
+    public class DateTimeWithFormatConverter : DateTimeConverterBase
+    {
+        private readonly string _format = "dd/MM/yyyy HH:mm:ss";
+
+        public DateTimeWithFormatConverter()
+        {
+
+        }
+
+        public DateTimeWithFormatConverter(string format)
+        {
+            _format = format;
+        }
+
+        /// <summary>
+        /// Writes value to JSON
+        /// </summary>
+        /// <param name="writer">JSON writer</param>
+        /// <param name="value">Value to be written</param>
+        /// <param name="serializer">JSON serializer</param>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((DateTime)value).ToString(_format));
+        }
+
+        /// <summary>
+        /// Reads value from JSON
+        /// </summary>
+        /// <param name="reader">JSON reader</param>
+        /// <param name="objectType">Target type</param>
+        /// <param name="existingValue">Existing value</param>
+        /// <param name="serializer">JSON serialized</param>
+        /// <returns>Deserialized DateTime</returns>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null) return null;
+
+            var s = reader.Value.ToString();
+            DateTime result;
+            if (DateTime.TryParseExact(s, _format, CultureInfo.InvariantCulture, DateTimeStyles.None, out result)) return result;
+
+            return DateTime.Now;
+        }
+    }
+}
---
+++ tests/UnitTestProjectCore/DateTimeWithFormatConverterTest.cs
@@ -0,0 +1,55 @@
+﻿using Helper.JsonConverters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+
+namespace UnitTestProjectCore
+{
+    internal class TesteConverterDefaultFormat
+    {
+        [JsonConverter(typeof(DateTimeWithFormatConverter))]
+        public DateTime Data { get; set; }
+    }
+
+    internal class TesteConverterCustomFormat
+    {
+        [JsonConverter(typeof(DateTimeWithFormatConverter), "dd-MM-yy HH:mm:ss")]
+        public DateTime Data { get; set; }
+    }
+
+
+    [TestClass]
+    public class DateTimeWithFormatConverterTest
+    {
+        [TestMethod]
+        public void DateTimeWithFormatConverter_UsingDefaultFormat()
+        {
+            string formatExpected = "dd/MM/yyyy HH:mm:ss";
+
+            var currentDate = DateTime.Now;
+
+            TesteConverterDefaultFormat teste = new TesteConverterDefaultFormat { Data = currentDate };
+
+            string json = JsonConvert.SerializeObject(teste);
+
+            Assert.IsTrue(json.Contains(currentDate.ToString(formatExpected)));
+
+        }
+
+        [TestMethod]
+        public void DateTimeWithFormatConverter_UsingCustomFormat()
+        {
+            string formatExpected = "dd-MM-yy HH:mm:ss";
+
+            var currentDate = DateTime.Now;
+
+            TesteConverterCustomFormat teste = new TesteConverterCustomFormat { Data = currentDate };
+
+            string json = JsonConvert.SerializeObject(teste);
+
+            Assert.IsTrue(json.Contains(currentDate.ToString(formatExpected)));
+
+        }
+
+    }
+}
